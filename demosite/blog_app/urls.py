c0b9ed970b878bfa53ae5086acc8084d81af02c7
Yusuke Seki <Yusuke1@YusukenoMacBook-Pro-3.local>
2022-11-04 07:41:44 +0900

    Build Docker image & Deploy to Amazon ECS

--- demosite/blog_app/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.FilmList.as_view(), name='home'),
-    path('<slug:slug>/', views.FilmDetail.as_view(), name='film_detail'),
-]
